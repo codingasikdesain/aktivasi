f4267171d2cda2cc2c1f70f5c41948b432a81671
codingasikdesain <108223112+codingasikdesain@users.noreply.github.com>
2023-10-14 04:05:22 +0700

    Create mutasi.js

---
+++ mutasi.js
@@ -0,0 +1,167 @@
+const _0x228853 = _0x4044;
+(function (_0x1bde05, _0x9ac5c) {
+    const _0xad682d = _0x4044,
+        _0x34b300 = _0x1bde05();
+    while (!![]) {
+        try {
+            const _0x341406 = parseInt(_0xad682d(0x1a2)) / 0x1 * (-parseInt(_0xad682d(0x1af)) / 0x2) + parseInt(_0xad682d(0x194)) / 0x3 * (-parseInt(_0xad682d(0x191)) / 0x4) + parseInt(_0xad682d(0x1ae)) / 0x5 * (-parseInt(_0xad682d(0x1b1)) / 0x6) + parseInt(_0xad682d(0x1ba)) / 0x7 * (-parseInt(_0xad682d(0x1a4)) / 0x8) + -parseInt(_0xad682d(0x176)) / 0x9 + parseInt(_0xad682d(0x173)) / 0xa + parseInt(_0xad682d(0x1a9)) / 0xb * (parseInt(_0xad682d(0x18b)) / 0xc);
+            if (_0x341406 === _0x9ac5c) break;
+            else _0x34b300['push'](_0x34b300['shift']());
+        } catch (_0x4d346f) {
+            _0x34b300['push'](_0x34b300['shift']());
+        }
+    }
+}(_0x1f51, 0xf379a));
+const _0x9e1904 = (function () {
+        let _0x3682cf = !![];
+        return function (_0xe0af83, _0x445f09) {
+            const _0x5d4c3c = _0x3682cf ? function () {
+                const _0x28dff2 = _0x4044;
+                if (_0x445f09) {
+                    const _0x2593a9 = _0x445f09[_0x28dff2(0x195)](_0xe0af83, arguments);
+                    return _0x445f09 = null, _0x2593a9;
+                }
+            } : function () {};
+            return _0x3682cf = ![], _0x5d4c3c;
+        };
+    }()),
+    _0xa6419f = _0x9e1904(this, function () {
+        const _0x858a13 = _0x4044;
+        return _0xa6419f['toString']()['search'](_0x858a13(0x19b))[_0x858a13(0x17f)]()['constructor'](_0xa6419f)['search'](_0x858a13(0x19b));
+    });
+
+function _0x1f51() {
+    const _0x137696 = ['54623888TkvOKb', 'Anda melakukan perubahan saldo melalui API (tambah)', 'aktif', '</h5>\x0a                  </div>\x0a                ', '&token_user=', '39055MuSnVe', '1205192TAygmf', 'Halaman ', '1086XLfaSi', '.list-page', 'show', 'text', 'Anda mengurangi saldo secara manual dari aplikasi bukaOlshop', '#detail_lastSaldo', 'Rp ', '{}.constructor(\"return this\")( )', 'match', '12558266trxEpe', '#detail_tanggal', 'Anda melakukan perubahan saldo melalui API (kurang)', '#selanjutnya', 'html', 'querySelectorAll', 'informasi_catatan', 'Admin Qita mengurangi dana anda', '</span>\x0a                      </div>\x0a                    </div>\x0a                    <h5 class=\"change-saldo\">Rp', '__proto__', 'removeClass', '16893930yNynrR', 'd-none', 'jumlah_dana', '8865738noMdJe', '#masuk', 'exception', 'bind', '#kembali', 'textContent', 'each', 'warn', 'info', 'toString', '#page', 'nonaktif', 'ready', '.list-transaksi', 'table', '</p>\x0a                        <span>', '#keluar', 'empty', '.history-none', 'error', 'trace', '12EeVDXu', 'join', '\x0a                  <div class=\"item\">\x0a                    <div class=\"menu\">\x0a                      <img src=\"https://i.ibb.co/rvZKyFK/card.png\" alt=\"\" style=\"background:#B71C1C\">\x0a                      <div>\x0a                        <p>', 'Anda telah menerima dana dari seseorang', 'console', 'append', '12ZOSHEe', 'click', 'reverse', '259824HXGrKU', 'apply', 'tanggal', 'id-ID', 'data', '#ubah-saldo', 'saldo_terakhir', '(((.+)+)+)+$', 'includes', 'length', 'toLocaleString', 'open', 'return (function() ', 'info-danger', '2EoLdwP', 'split', '8WklqPO', 'Anda menerima dana dari admin Qita', 'Anda mengkonfirmasi topup yang dilakukan member', 'addClass', 'hide'];
+    _0x1f51 = function () {
+        return _0x137696;
+    };
+    return _0x1f51();
+}
+
+function _0x4044(_0x37412d, _0x1f4c9c) {
+    const _0x22016f = _0x1f51();
+    return _0x4044 = function (_0xf53fb9, _0x353187) {
+        _0xf53fb9 = _0xf53fb9 - 0x16e;
+        let _0x1d4ad4 = _0x22016f[_0xf53fb9];
+        return _0x1d4ad4;
+    }, _0x4044(_0x37412d, _0x1f4c9c);
+}
+_0xa6419f();
+const _0x353187 = (function () {
+        let _0x57b8f8 = !![];
+        return function (_0x47027b, _0x4b0087) {
+            const _0x73e7ad = _0x57b8f8 ? function () {
+                const _0x50cca0 = _0x4044;
+                if (_0x4b0087) {
+                    const _0x31d30b = _0x4b0087[_0x50cca0(0x195)](_0x47027b, arguments);
+                    return _0x4b0087 = null, _0x31d30b;
+                }
+            } : function () {};
+            return _0x57b8f8 = ![], _0x73e7ad;
+        };
+    }()),
+    _0xf53fb9 = _0x353187(this, function () {
+        const _0x436f25 = _0x4044;
+        let _0xed9e0c;
+        try {
+            const _0x238f9e = Function(_0x436f25(0x1a0) + _0x436f25(0x1b8) + ');');
+            _0xed9e0c = _0x238f9e();
+        } catch (_0x19f4fd) {
+            _0xed9e0c = window;
+        }
+        const _0x21f78a = _0xed9e0c[_0x436f25(0x18f)] = _0xed9e0c['console'] || {},
+            _0x52ec70 = ['log', _0x436f25(0x17d), _0x436f25(0x17e), _0x436f25(0x189), _0x436f25(0x178), _0x436f25(0x184), _0x436f25(0x18a)];
+        for (let _0x342f32 = 0x0; _0x342f32 < _0x52ec70[_0x436f25(0x19d)]; _0x342f32++) {
+            const _0x2210b0 = _0x353187['constructor']['prototype'][_0x436f25(0x179)](_0x353187),
+                _0x30a4d7 = _0x52ec70[_0x342f32],
+                _0x19d736 = _0x21f78a[_0x30a4d7] || _0x2210b0;
+            _0x2210b0[_0x436f25(0x171)] = _0x353187['bind'](_0x353187), _0x2210b0['toString'] = _0x19d736[_0x436f25(0x17f)][_0x436f25(0x179)](_0x19d736), _0x21f78a[_0x30a4d7] = _0x2210b0;
+        }
+    });
+_0xf53fb9(), $(document)[_0x228853(0x182)](() => {
+    const _0x26fe15 = _0x228853;
+    let _0x334afd, _0x2e9fa1 = 0x1;
+    const _0x4b308a = _0x2b80b3 => {
+            const _0x2cf471 = _0x4044;
+            $['ajax']({
+                'url': 'https://openapi.bukaolshop.net/v1/user/catatan?token=' + token + _0x2cf471(0x1ad) + token_user + '&id_user=' + id_user + '&tipe=saldo&page=' + _0x2b80b3,
+                'dataType': 'json',
+                'success': _0x1124eb => {
+                    const _0x23c5e5 = _0x2cf471;
+                    _0x334afd = _0x1124eb, _0x334afd[_0x23c5e5(0x198)]['length'] > 0x0 ? ($(_0x23c5e5(0x188))['hide'](), $(_0x23c5e5(0x180))[_0x23c5e5(0x172)](_0x23c5e5(0x174))) : $('.history-none')[_0x23c5e5(0x1b3)](), _0x468cf2();
+                },
+                'error': _0x5a941f => {
+                    const _0x4161b6 = _0x2cf471;
+                    $(_0x4161b6(0x188))[_0x4161b6(0x1b3)](), $('.page')[_0x4161b6(0x1a8)](), Toast[_0x4161b6(0x19f)](_0x5a941f, 0xbb8, _0x4161b6(0x1a1));
+                }
+            });
+        },
+        _0x58c8e2 = '<div class=\"detail-pembelian\">\x0a        <h6 class=\"dplT\">Detail Perubahan</h6>\x0a        <div class=\"detail-list\">\x0a            <div>\x0a                <p>Jumlah</p>\x0a                <h6 id=\"detail_jumlah\">none</h6>\x0a            </div>\x0a            <div>\x0a                <p>Saldo Terakhir</p>\x0a                <h6 id=\"detail_lastSaldo\">none</h6>\x0a            </div>\x0a            <div>\x0a                <p>Tanggal</p>\x0a                <h6 id=\"detail_tanggal\">none</h6>\x0a            </div>\x0a        </div>\x0a        <div class=\"catatan\">\x0a            <p>Catatan :</p>\x0a            <h6 id=\"detail_catan\"></h6>\x0a        </div>\x0a    </div>',
+        _0x2e90e9 = (_0x2aeff3, _0x6654c8, _0x3885fe, _0x118b6f) => {
+            const _0x4a0399 = _0x4044;
+            Sheet['open'](_0x58c8e2, styleForSheet), $('#detail_jumlah')[_0x4a0399(0x1be)](_0x4a0399(0x1b7) + _0x6654c8), $(_0x4a0399(0x1b6))[_0x4a0399(0x1be)]('Rp ' + _0x3885fe), $(_0x4a0399(0x1bb))[_0x4a0399(0x1be)](_0x118b6f), $('#detail_catan')['html'](_0x2aeff3);
+        },
+        _0x468cf2 = () => {
+            const _0x39d921 = _0x4044;
+            $(_0x39d921(0x183))[_0x39d921(0x187)](), $['each'](_0x334afd[_0x39d921(0x198)], (_0x527d3e, _0x143ca3) => {
+                const _0x3f118a = _0x39d921;
+                if (_0x143ca3['tipe_jumlah'] === 'tambah') {
+                    const _0x109b70 = parseFloat(_0x143ca3[_0x3f118a(0x175)])['toLocaleString'](_0x3f118a(0x197)),
+                        _0x4f8fee = parseFloat(_0x143ca3[_0x3f118a(0x19a)])[_0x3f118a(0x19e)](_0x3f118a(0x197));
+                    let _0x584079 = _0x143ca3['informasi_catatan'];
+                    if (_0x584079[_0x3f118a(0x19c)](_0x3f118a(0x1aa))) _0x584079 = _0x3f118a(0x18e);
+                    else {
+                        if (_0x584079[_0x3f118a(0x19c)]('Anda menambahkan saldo secara manual dari aplikasi bukaOlshop')) _0x584079 = _0x3f118a(0x1a5);
+                        else _0x584079['includes'](_0x3f118a(0x1a6)) && (_0x584079 = 'Top-Up saldo di konfirmasi oleh admin');
+                    }
+                    const _0x3749a2 = '\x0a                  <div class=\"item\">\x0a                    <div class=\"menu\">\x0a                      <img src=\"https://i.ibb.co/rvZKyFK/card.png\" alt=\"\" style=\"background:#01A6AC\">\x0a                      <div>\x0a                        <p>' + _0x584079 + _0x3f118a(0x185) + _0x143ca3[_0x3f118a(0x196)] + _0x3f118a(0x170) + _0x109b70 + _0x3f118a(0x1ac),
+                        _0x106c86 = $(_0x3749a2);
+                    _0x106c86['on'](_0x3f118a(0x192), () => {
+                        const _0x19ffe8 = _0x3f118a;
+                        _0x2e90e9(_0x584079, _0x109b70, _0x4f8fee, _0x143ca3[_0x19ffe8(0x196)]);
+                    }), $(_0x3f118a(0x183))[_0x3f118a(0x190)](_0x106c86);
+                }
+            }), $(_0x39d921(0x1b2))['text']('Halaman ' + _0x2e9fa1), _0x2e9fa1 > 0x1 ? $(_0x39d921(0x17a))[_0x39d921(0x1b3)]() : $(_0x39d921(0x17a))[_0x39d921(0x1a8)]();
+        },
+        _0x353ad7 = () => {
+            const _0x436046 = _0x4044;
+            $('.list-transaksi')['empty'](), $[_0x436046(0x17c)](_0x334afd[_0x436046(0x198)], (_0x677cce, _0x3355a7) => {
+                const _0x59d309 = _0x436046;
+                if (_0x3355a7['tipe_jumlah'] === 'kurang') {
+                    const _0x510af9 = parseFloat(_0x3355a7[_0x59d309(0x175)])[_0x59d309(0x19e)]('id-ID'),
+                        _0xe0aa06 = parseFloat(_0x3355a7[_0x59d309(0x19a)])[_0x59d309(0x19e)]('id-ID');
+                    let _0x3da4b0 = _0x3355a7[_0x59d309(0x16e)];
+                    if (_0x3da4b0[_0x59d309(0x19c)](_0x59d309(0x1bc))) _0x3da4b0 = _0x59d309(0x18e);
+                    else _0x3da4b0[_0x59d309(0x19c)](_0x59d309(0x1b5)) && (_0x3da4b0 = _0x59d309(0x16f));
+                    const _0x28d270 = _0x59d309(0x18d) + _0x3da4b0 + '</p>\x0a                        <span>' + _0x3355a7[_0x59d309(0x196)] + _0x59d309(0x170) + _0x510af9 + _0x59d309(0x1ac),
+                        _0x3d690e = $(_0x28d270);
+                    _0x3d690e['on'](_0x59d309(0x192), () => {
+                        _0x2e90e9(_0x3da4b0, _0x510af9, _0xe0aa06, _0x3355a7['tanggal']);
+                    }), $('.list-transaksi')[_0x59d309(0x190)](_0x3d690e);
+                }
+            }), $('.list-page')[_0x436046(0x1b4)](_0x436046(0x1b0) + _0x2e9fa1);
+        };
+    _0x4b308a(_0x2e9fa1), $(_0x26fe15(0x177))['click'](() => {
+        const _0x215ddb = _0x26fe15;
+        $(_0x215ddb(0x177))[_0x215ddb(0x172)](_0x215ddb(0x181)), $(_0x215ddb(0x177))[_0x215ddb(0x1a7)](_0x215ddb(0x1ab)), $(_0x215ddb(0x186))[_0x215ddb(0x172)](_0x215ddb(0x1ab)), $('#keluar')[_0x215ddb(0x1a7)]('nonaktif'), $(_0x215ddb(0x1bd))[_0x215ddb(0x1b3)](), $(_0x215ddb(0x17a))['show'](), _0x468cf2();
+    }), $(_0x26fe15(0x186))[_0x26fe15(0x192)](() => {
+        const _0x3ee302 = _0x26fe15;
+        $(_0x3ee302(0x177))[_0x3ee302(0x172)](_0x3ee302(0x1ab)), $(_0x3ee302(0x177))['addClass'](_0x3ee302(0x181)), $('#keluar')[_0x3ee302(0x172)]('nonaktif'), $('#keluar')[_0x3ee302(0x1a7)](_0x3ee302(0x1ab)), $(_0x3ee302(0x1bd))['hide'](), $('#kembali')['hide'](), _0x353ad7();
+    }), $(_0x26fe15(0x1bd))[_0x26fe15(0x192)](() => {
+        _0x2e9fa1++, _0x4b308a(_0x2e9fa1);
+    }), $(_0x26fe15(0x17a))[_0x26fe15(0x192)](() => {
+        _0x2e9fa1 > 0x1 && (_0x2e9fa1--, _0x4b308a(_0x2e9fa1));
+    });
+    var _0x4f80a1 = document[_0x26fe15(0x1bf)](_0x26fe15(0x199));
+
+    function _0x1c3d16(_0x59423c) {
+        const _0x3c64cf = _0x26fe15;
+        var _0x557858 = _0x59423c['toString']()[_0x3c64cf(0x1a3)]('')['reverse']()['join'](''),
+            _0x825be5 = _0x557858[_0x3c64cf(0x1b9)](/\d{1,3}/g);
+        return _0x825be5 = _0x825be5[_0x3c64cf(0x18c)]('.')['split']('')[_0x3c64cf(0x193)]()[_0x3c64cf(0x18c)](''), _0x3c64cf(0x1b7) + _0x825be5;
+    }
+    _0x4f80a1['forEach'](function (_0x862801) {
+        const _0x4a799e = _0x26fe15;
+        _0x862801[_0x4a799e(0x17b)] = _0x1c3d16(_0x862801[_0x4a799e(0x17b)]);
+    });
+});
